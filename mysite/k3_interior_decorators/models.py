b41be8a2cdf281191d1c8dd9c6c102dc351dddcb
nayanait <balaprem1969@icloud.com>
2021-01-02 09:56:10 -0800

    k3 interior decorators integrated with Django & Pycharm

--- mysite/k3_interior_decorators/models.py
+++ mysite/k3_interior_decorators/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem:
+    id:int
+    name:str
+    desc:str
--- mysite/k3_interior_decorators/views.py
+++ mysite/k3_interior_decorators/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import MenuItem
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    menuitem1 = MenuItem()
+    menuitem1.name = 'Home'
+    menuitem1.desc = 'This is the Home page'
+
+    menuitem2 = MenuItem()
+    menuitem2.name = 'About'
+    menuitem2.desc = 'This is the About page'
+
+    menuitem3 = MenuItem()
+    menuitem3.name = 'Services'
+    menuitem3.desc = 'This is the Services page'
+
+    menuitem4 = MenuItem()
+    menuitem4.name = 'Portfolio'
+    menuitem4.desc = 'This is the Portfolio page'
+
+    menuitem5 = MenuItem()
+    menuitem5.name = 'Contacts'
+    menuitem5.desc = 'This is the Contacts page'
+
+    menuitems = [menuitem1, menuitem2, menuitem3, menuitem4, menuitem5]
+
+    return render( request, 'index.html', {'menuitems':menuitems});
